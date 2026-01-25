09e32e971d45e25ceb1ab78a433edcf40501341d
Ajay <imogamingarena@gmail.com>
2026-01-25 19:51:53 +0530

    updated port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 // basic route
 app.get('/', (req: Request, res: Response) => {
